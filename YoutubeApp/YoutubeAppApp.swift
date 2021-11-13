0c27e4f8114fbb22037c7b9d9e354662d51f7ec2
Ed <76992905+edphan@users.noreply.github.com>
2021-11-12 17:23:23 -0800

    Initial Commit

---
+++ YoutubeApp/YoutubeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeAppApp.swift
+//  YoutubeApp
+//
+//  Created by Edward Phan on 2021-11-12.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
